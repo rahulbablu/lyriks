198d27a77de6d72da6a621ad5ab594f2fb4c9904
rahulbablu <bablu.kounjula@gmail.com>
2023-01-08 01:06:03 +0530

    fixed base url

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://shazam-core.p.rapidapi.com",
+    baseUrl: "https://shazam-core.p.rapidapi.com/",
     prepareHeaders: (headers) => {
       headers.set(
         "X-RapidAPI-Key",
@@ -13,25 +13,25 @@ export const shazamCoreApi = createApi({
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => "/v1/charts/world" }),
+    getTopCharts: builder.query({ query: () => "v1/charts/world" }),
     getSongsByGenre: builder.query({
-      query: (genre) => `/v1/charts/genre-world?genre_code=${genre}`,
+      query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
     }),
     getSongDetails: builder.query({
-      query: (songid) => `/v1/tracks/details?track_id=${songid}`,
+      query: (songid) => `v1/tracks/details?track_id=${songid}`,
     }),
     getSongRelated: builder.query({
-      query: (songid) => `/v1/tracks/related?track_id=${songid}`,
+      query: (songid) => `v1/tracks/related?track_id=${songid}`,
     }),
     getArtistDetails: builder.query({
-      query: (artistId) => `/v2/artists/details?artist_id=${artistId}`,
+      query: (artistId) => `v2/artists/details?artist_id=${artistId}`,
     }),
     getSongsByCountry: builder.query({
       query: (countryCode) =>
-        `/v1/charts/country?country_code=${countryCode ? countryCode : "IN"}`,
+        `v1/charts/country?country_code=${countryCode ? countryCode : "IN"}`,
     }),
     getSongsBySearch: builder.query({
-      query: (searchTerm) => `/v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+      query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
     }),
   }),
 });
