d04b486edade652a422bc5bc7f3466beb993963b
rahulbablu <bablu.kounjula@gmail.com>
2023-01-10 01:15:56 +0530

    fixed discover by flex start to center

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -37,7 +37,7 @@ const Discover = () => {
         </select>
       </div>
 
-      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+      <div className="w-full flex flex-wrap justify-center gap-8">
         {data?.map((song, i) => (
           <SongCard
             key={song.key}
